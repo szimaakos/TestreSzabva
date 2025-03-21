bd1bd4553e3f8b3104680148eb896bc16978508f
Sutak0 <sutak.david.5@gmail.com>
2025-03-21 13:13:50 +0100

    email confirmed oldal szepitesss

--- frontend/src/components/EmailConfirmed.tsx
+++ frontend/src/components/EmailConfirmed.tsx
@@ -1,13 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { CheckCircle } from "lucide-react";
+import "./EmailConfirmed.css";
 
 const EmailConfirmed = () => {
   return (
     <div className="email-confirmed-container">
       <div className="email-confirmed-content">
+        <div className="success-icon">
+          <CheckCircle size={64} />
+        </div>
         <h1>E-mail cím megerősítve!</h1>
-        <p>Köszönjük, hogy megerősítetted az e-mail címedet.</p>
-        <p>Most már bejelentkezhetsz a TestreSzabva alkalmazásba.</p>
+        <p className="success-message">Köszönjük, hogy megerősítetted az e-mail címedet.</p>
+        <p>Most már bejelentkezhetsz a TestreSzabva alkalmazásba és élvezheted az összes funkciót.</p>
         <Link to="/" className="login-link">
           Tovább a bejelentkezéshez
         </Link>
